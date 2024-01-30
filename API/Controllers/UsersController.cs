2d0ae87b09869d3cf6071fa7ceb6880cbb6f73ad
Michal Ozog <mozog@etobres.pl>
2024-01-30 14:39:17 +0100

    init file

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data; // to odnosi sie do DataContext
+using API.Entities; //AppUser Entitie z naszego pliku AppUser
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+// route dla controllera api/users
+[Route("api/[controller]")]
+
+public class UsersController: ControllerBase
+{
+private readonly DataContext _context;
+        public UsersController(DataContext context){
+        _context = context;
+        }
+
+        // zwraca cala tablice z users.. api/users
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+     // zwraca cala tablice z users o podanym ID.. api/users/2
+    [HttpGet("{id}")]
+public async Task<ActionResult<AppUser>> GetUser(int id)
+{
+    var user = await _context.Users.FindAsync(id);
+
+    if (user == null)
+    {
+        return NotFound(); // Return 404 Not Found if user with the specified ID is not found
+    }
+
+    return user;
+}
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿// to jest importowanie App User z API.ENtities uzera do naszego DbSet<Appuser>
+using API.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+    public DbSet<AppUser> Users { get; set; }
+
+}
